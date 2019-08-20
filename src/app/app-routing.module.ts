5bedfad0875a380371aff457d49d14cea0e7bc21
Vishnuvardhan Prem <vishnuvardhan.prem@gmail.com>
2019-08-20 10:25:07 +0300

    Added Backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
+import { ManageLocationComponent } from './pages/manage-location/manage-location.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapviewComponent } from './pages/mapview/mapview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ManageLocationComponent},
+  {path:"mapp",component:MapviewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
+import { LocationdataService } from './service/locationdata.service';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { ManageLocationComponent } from './pages/manage-location/manage-location.component';
+import { MapviewComponent } from './pages/mapview/mapview.component';
+import { TableComponent } from './components/table/table.component';
+import { AgmCoreModule } from '@agm/core';
+import { ModalComponent } from './components/modal/modal.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    ManageLocationComponent,
+    MapviewComponent,
+    TableComponent,
+    ModalComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+    apiKey: 'AIzaSyCHG3Sbr1YJDjmWzHXXgqzELPovbjQkeao'
+    })
   ],
-  providers: [],
+  entryComponents:[ModalComponent],
+  providers: [NgbActiveModal],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
